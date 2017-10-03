e8eb7b1370faedb6b919c018cded39e15ddd5915
laDanz <cdanzmann@gmail.com>
2017-10-03 13:16:49 +0200

    get naschies

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,7 @@ var clay = new Clay(clayConfig, null, {autoHandleEvents: false});
 var NASCHPUNKTE_URL="https://naschpunkte.appspot.com/";
 var USER_ID = Settings.option('user_id') || undefined;
 var ACTIVITIES = Settings.option('activities') || undefined;
+var NASCHIES = Settings.option('naschies') || undefined;
 
 // clay settings
 Pebble.addEventListener('showConfiguration', function(e) {
@@ -52,16 +53,113 @@ var activityMenu = new UI.Menu({
   activityMenu.on('select', function(e) {
     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
     console.log('The item is titled "' + e.item.title + '"');
+    var value = e.item.entity.defaultValue;
+    var card = new UI.Card();
+    card.title(e.item.title);
+    card.subtitle(e.item.subtitle);
+    card.body(value + " " + e.item.entity.einheit + ' ?');
+    card.show();
+    card.on('click', 'select', function(event) {
+      console.log("adding " + value + e.item.entity.einheit + " of " + e.item.entity.name + "(" + JSON.stringify(e.item.entity)+")");
+      createEvent(e.item.entity.urlsafe, value);
+      activityMenu.hide();
+      card.hide();
+    });
+    function updateValue(){
+      card.body(value + " " + e.item.entity.einheit + ' ?');
+    }
+    card.on('click', 'up', function(e) {
+      if (value>=1){
+        value++;
+      }else{
+        value+=0.25;  
+      }
+      updateValue();
+    });
+    card.on('click', 'down', function(e) {
+      if (value>1){
+        value--;
+      }else if (value == 0){}else{
+        value-=0.25;
+      }
+      updateValue();
+    });
+    
   });
+
+main.on('click', 'up', function(e) {
+  updateActivityMenu();
+  activityMenu.show();
+});
 
 function updateActivityMenu(){
   var data = Settings.option('activities');
     var items = [];
     for (var i = 0; i < data.length; i++){
       var obj = data[i];
-      items.push({title: obj.name, subtitle: obj.punkte + "pkt pro " + obj.defaultValue + " " + obj.einheit});
+      items.push({title: obj.name, subtitle: obj.punkte + "pkt pro " + obj.defaultValue + " " + obj.einheit, entity: obj});
     }
     activityMenu.items(0, items);
+}
+
+var naschMenu = new UI.Menu({
+    sections: [{
+      items: [{
+        title: 'Naschies',
+        subtitle: 'hole...'
+      }]
+    }]
+  });
+  naschMenu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
+    var value = e.item.entity.defaultValue;
+    var card = new UI.Card();
+    card.title(e.item.title);
+    card.subtitle(e.item.subtitle);
+    card.body(value + " " + e.item.entity.einheit + ' ?');
+    card.show();
+    card.on('click', 'select', function(event) {
+      console.log("adding " + value + e.item.entity.einheit + " of " + e.item.entity.name + "(" + JSON.stringify(e.item.entity)+")");
+      createEvent(e.item.entity.urlsafe, value);
+      naschMenu.hide();
+      card.hide();
+    });
+    function updateValue(){
+      card.body(value + " " + e.item.entity.einheit + ' ?');
+    }
+    card.on('click', 'up', function(e) {
+      if (value>=1){
+        value++;
+      }else{
+        value+=0.25;  
+      }
+      updateValue();
+    });
+    card.on('click', 'down', function(e) {
+      if (value>1){
+        value--;
+      }else if (value == 0){}else{
+        value-=0.25;
+      }
+      updateValue();
+    });
+    
+  });
+
+main.on('click', 'down', function(e) {
+  updateNaschMenu();
+  naschMenu.show();
+});
+
+function updateNaschMenu(){
+  var data = Settings.option('naschies');
+    var items = [];
+    for (var i = 0; i < data.length; i++){
+      var obj = data[i];
+      items.push({title: obj.name, subtitle: obj.punkte + "pkt pro " + obj.defaultValue + " " + obj.einheit, entity: obj});
+    }
+    naschMenu.items(0, items);
 }
 
 // ajax requests
@@ -112,6 +210,46 @@ if (ACTIVITIES === undefined){
   updateActivities();
 }
 
+function updateNaschies() {
+  if (USER_ID === undefined){
+    login();
+  }
+  ajax({ url: NASCHPUNKTE_URL+'rest/lsn/?user_id='+USER_ID, type:"json"},
+  function(data) {
+    console.log('updateNaschiesCall successful!' + data);
+    Settings.option('naschies', data);
+    updateNaschMenu();
+  },
+  function(data, code) {
+    console.log('Error getting naschies: ' + code + ": " + data);
+    var title="Fehler!";
+    var subtitle="";
+    if (code == 401){
+      subtitle="Nicht angemeldet!";
+    }
+    naschMenu.items(0, [{title: title, subtitle: subtitle}]);
+  }
+  );
+}
+if (NASCHIES === undefined){
+  updateNaschies();
+}
+
+function createEvent(event, value) {
+  if (USER_ID === undefined){
+    login();
+  }
+  var data={user_id: USER_ID, event:event, value:value};
+  ajax({ url: NASCHPUNKTE_URL+'rest/cre/', data: data, method: "post"},
+  function(data) {
+    console.log('createEvent successful!');
+    updatePunkte();
+  },
+  function(data, code) {
+    console.log('Error creatingEvent: ' + code + ": " + data);
+  });
+}
+
 function updatePunkte() {
   if (USER_ID === undefined){
     login();
@@ -134,12 +272,9 @@ updatePunkte();
 
 main.show();
 
-main.on('click', 'up', function(e) {
-  updateActivityMenu();
-  activityMenu.show();
-});
-
 main.on('click', 'select', function(e) {
+  updateActivities();
+  updateNaschies();
   var wind = new UI.Window({
     backgroundColor: 'black'
   });
@@ -174,12 +309,4 @@ main.on('click', 'select', function(e) {
   wind.add(radial);
   wind.add(textfield);
   wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
 });
